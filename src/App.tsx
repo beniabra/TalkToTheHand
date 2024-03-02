ea1dc3f4e4e9a41d6ffb75e869809d471ca6d141
aparnaroy <aparnar@udel.edu>
2024-03-02 16:35:21 -0500

    fixed navbar

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,19 @@ import {
   Tab,
   TabPanel,
   Flex,
+  Grid,
 } from "@chakra-ui/react"
 //import { ColorModeSwitcher } from "./ColorModeSwitcher"
 //import { Logo } from "./Logo"
 import Dictaphone from './Dictaphone';
 import { Home } from "./Home";
+import { Image } from "@chakra-ui/react";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl" backgroundColor="#d9fff7">
 
-      <Tabs variant='soft-rounded' colorScheme='white'>
+      <Tabs variant='soft-rounded' colorScheme='teal'>
         <Flex
               as="nav"
               align="center"
@@ -29,15 +31,16 @@ export const App = () => (
               bgGradient="linear(to-b, #167875, #d9fff7)"
               color="white"
         >
-          <TabList paddingTop="4" paddingBottom="4" color="white">
-            <Flex align="center" ml={5} mr={10}>
-              Logo
-            </Flex>
-            
-              <Tab color="#ffffff">Home</Tab>
-              <Tab color="#ffffff">Speech to ASL Converter</Tab>
-              <Tab color="#ffffff">Learn ASL</Tab>
-              <Tab color="#ffffff">About</Tab>
+          <Flex align="center" ml={5} mr={10}>
+            <Image src="hand-waving.png" alt="Logo" w="100px" h="auto" />
+          </Flex>
+          <TabList paddingTop="4" paddingBottom="4" color="white" style={{ marginLeft: "auto" }}>
+            <Grid templateColumns='repeat(4, 1fr)'>
+              <Tab color="#ffffff" _hover={{ color: '#000000', backgroundColor: '#ffffff' }} >Home</Tab>
+              <Tab color="#ffffff" _hover={{ color: '#000000', backgroundColor: '#ffffff' }}>Speech to ASL Converter</Tab>
+              <Tab color="#ffffff" _hover={{ color: '#000000', backgroundColor: '#ffffff' }}>Learn ASL</Tab>
+              <Tab color="#ffffff" _hover={{ color: '#000000', backgroundColor: '#ffffff' }}>About</Tab>
+            </Grid>
           </TabList>
         </Flex>
 
