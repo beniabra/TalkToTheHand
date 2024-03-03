fb858ba73fde360f1d3e77b65dab0f782a952240
aparnaroy <aparnar@udel.edu>
2024-03-02 22:53:55 -0500

    finished ui of translate page

--- src/Converter.tsx
+++ src/Converter.tsx
@@ -3,16 +3,17 @@ import { Text, Box } from '@chakra-ui/react';
 import Dictaphone from './Dictaphone';
 
 export function Converter() {
-
-    
-
     return (
       <div>
-        <Box padding={"100px"} h="100vh" bg="#d9fff7">
-        <Text fontSize="3xl" textAlign="center" >
-            Hi, this is our Converter.
-        </Text>
-        <Dictaphone></Dictaphone>
+        <Box padding={"5px"} h="100vh" bg="#d9fff7">
+          <Text fontSize="4xl" textAlign="center" >
+              Hi, this is our Translator
+          </Text>
+          <Text padding={"5px"} fontSize="3m" textAlign="center" >
+              Instantly translate any speech or text into ASL.
+          </Text>
+          <Box width="100%" py={5} textAlign="center"></Box>
+          <Dictaphone></Dictaphone>
         </Box>
       </div>
     );
